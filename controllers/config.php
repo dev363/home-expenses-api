dfb1dbaa6efcbd32308d81edaa7d1514b5ed3400
sat363 <sat363@gmail.com>
2023-08-11 17:41:28 +0530

    Db connection , Category Api Done

---
+++ controllers/config.php
@@ -0,0 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'home_expenses');
